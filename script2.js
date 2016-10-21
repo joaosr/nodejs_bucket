3a3eff1039fefa843b02330d3816342b26e728fa
joaosr <joao.soares@terras.agr.br>
2016-10-21 13:09:16 -0300

    updated simple server request method

--- script2.js
+++ script2.js
@@ -1,10 +1,15 @@
-var server = require('http');
+var http = require('http');
+var server = http.createServer(engine);
+server.listen(1337, function(){
+	console.log("Server was hit by a request");
+});
 
-server.createServer(engine).listen(1337);
+// server.createServer(engine).listen(1337); // leets
 
 function engine(request, response) {
 	// body...
+	//console.log(response);
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 
-	response.end('Hey there, from the server! :D');
+	response.end('Hey there, from the server! :D. Request from page: ' + request.url);
 }
